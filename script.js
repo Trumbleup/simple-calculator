dc035718cbf7151c0ddbae7c5a2949009ca34a4a
Danny <Dtrumble12@gmail.com>
2020-09-18 19:23:41 -0500

    Working on the logic of the calculator, including displaying a single number, and applying conditionals for the main operation function

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function operate (a, b, operator) {
-	return operator(a, b)
+	const parsedA = parseInt(a);
+	const parsedB = parseInt(b);
+	if (operator == '+') {
+		return add(parsedA,parsedB);
+	} else if (operator == '-') {
+		return subtract(parsedA,parsedB);
+	} else if (operator == '*') {
+		return multiply(parsedA,parsedB);
+	} else if (operator == '/') {
+		return divide(parsedA,parsedB);
+	}
 }
 
 function add (a, b) {
@@ -18,12 +28,36 @@ function divide (a, b) {
 	return a/b
 }
 
+const display = document.getElementById('display');
 const buttons = document.querySelectorAll('.button');
+let operationArray = [];
+let lastAnswer = 0;
 buttons.forEach(button => {
 	button.addEventListener('click', () => {
 		const value = button.getAttribute('value');
-		const display = document.getElementById('display');
-		display.innerHTML += value;
+		if (value == '=') {		// if the '=' button is pressed, evaluate the final 2 number operation
+			const refArray = operationArray;
+			const firstValue = refArray[0];
+			const operator = refArray[1];
+			const lastValue = refArray[2];
+			const newAnswer = operate(firstValue, lastValue, operator);
+			display.innerHTML = newAnswer;
+			operationArray = []; 
+		} else if (value == '+' || value == '-' || value == '*' || value == '/') { // if an operator button is pressed
+			if (operationArray.length == 2) { // if the operation array already has 2 numbers
+				const refArray = operationArray;
+				const firstValue = refArray[0];
+				const operator = refArray[1];
+				const lastValue = refArray[2];
+				const newAnswer = operate(firstValue, lastValue, operator);
+				operationArray = [...newAnswer];
+				display.innerHTML = newAnswer;
+			}
+			operationArray.push(value);
+		} else { // for all numbers and decimals
+			operationArray.push(value);
+			display.innerHTML = value;
+		}
 	})
 })
 
